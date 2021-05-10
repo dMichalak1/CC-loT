53935da593bd0e86a50297722eff7bb0db5c31fc
dMichalak1 <48916443+dMichalak1@users.noreply.github.com>
2021-05-10 20:28:30 +0200

    loT 3

---
+++ loT/azurefn/Person.cs
@@ -0,0 +1,10 @@
+namespace cdv.people{
+      public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phonenumber { get; set; }
+        public string PhoneNumber { get; internal set; }
+    }
+}
