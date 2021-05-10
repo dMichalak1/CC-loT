a32874868be2e09ff9654f806e40e368cf4be49a
dMichalak1 <48916443+dMichalak1@users.noreply.github.com>
2021-05-10 15:54:27 +0200

    loT

---
+++ loT/FirstTimeWithAzure/PeopleWebApp/Controllers/PeopleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleDb db;
+
+        public PeopleController(PeopleDb db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var people = db.People.ToList();
+        
+            return Ok(people);
+        }
+    }
+    
+}
