c945e60ee755927504558b08fdc6ad4b8b125bf2
dMichalak1 <48916443+dMichalak1@users.noreply.github.com>
2021-05-12 00:39:03 +0200

    mobile_3_and_4

---
+++ Xamarin/PeopleStoreApp/PeopleStoreApp/PeopleStoreApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using PeopleStoreApp.DataContracts;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PeopleStoreApp
+{
+    public partial class App : Application
+    {
+        private const string API_URL = "http://192.168.1.16:45024";
+        public App()
+        {
+            InitializeComponent();
+
+            var client = RestEase.RestClient.For<IPeopleClient>("http://192.168.1.4:6001/api");
+         
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
